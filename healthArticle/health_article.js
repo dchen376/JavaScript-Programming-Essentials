ef1f9fbcbeb29b7cdfb57f844510d7b82710a35a
dchen376 <dongping07>
2024-11-28 20:50:20 -0500

    JSON, XMLHttpRequest()

---
+++ healthArticle/health_article.js
@@ -0,0 +1,67 @@
+//  create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest(); //  allows you to send asynchronous HTTP requests to a server.
+
+// url to define the URL of the JSON file
+var url = './health_article.json';
+
+//  GET request to the specified URL
+xhr.open('GET', url, true); // True: Indicates if the request is asynchronous (true)
+
+//  to inform the XMLHttpRequest object that the expected response from the server should be in JSON format.
+xhr.responseType = 'json';
+
+// what should happen when the data is successfully loaded using
+xhr.onload = function(){
+
+    var articles = xhr.response.articles;
+    var articlesDiv = document.getElementById('articles');
+
+    // Iterate through articles and constructing HTML:
+articles.forEach(function(article) {
+    var articleDiv = document.createElement('div');
+    articleDiv.classList.add('article');
+
+    var title = document.createElement('h2');
+    title.textContent = article.title;
+
+    var description = document.createElement('p');
+    description.textContent = article.description;
+
+    var waysHeader = document.createElement('h3');
+    waysHeader.textContent = 'Ways to Achieve:';
+
+    var waysList = document.createElement('ul');
+    article.ways_to_achieve.forEach(function(way) {
+      var listItem = document.createElement('li');
+      listItem.textContent = way;
+      waysList.appendChild(listItem);
+    });
+
+    var benefitsHeader = document.createElement('h3');
+    benefitsHeader.textContent = 'Benefits:';
+
+    var benefitsList = document.createElement('ul');
+    article.benefits.forEach(function(benefit) {
+      var listItem = document.createElement('li');
+      listItem.textContent = benefit;
+      benefitsList.appendChild(listItem);
+    });
+
+    articleDiv.appendChild(title);
+    articleDiv.appendChild(description);
+    articleDiv.appendChild(waysHeader);
+    articleDiv.appendChild(waysList);
+    articleDiv.appendChild(benefitsHeader);
+    articleDiv.appendChild(benefitsList);
+
+    articlesDiv.appendChild(articleDiv);
+  });
+
+
+}
+
+
+
+
+  // send the XMLHttpRequest to fetch the data from the specified URL
+  xhr.send();
